843caf3dde0717848c2f0685fbb85b8e2d93cea1
DriverStation <frcteam28@gmail.com>
2017-05-31 16:40:22 -0400

    Changed Code

--- src/org/usfirst/frc/team28/robot/Movement.java
+++ src/org/usfirst/frc/team28/robot/Movement.java
@@ -3,11 +3,12 @@ package org.usfirst.frc.team28.robot;
 import edu.wpi.first.wpilibj.RobotDrive;
 
 public class Movement {
-	private Rotaion rotaion = new Rotaion(0);
-	private RobotDrive
+	private Rotaion rotaion = new Rotaion(PinConstants.GYRO_PIN);
+	private RobotDrive robotDrive = new RobotDrive(PinConstants.FL_MOTOR, PinConstants.BL_MOTOR, PinConstants.FR_MOTOR, PinConstants.BR_MOTOR);
 	
 	public void update(){
 		rotaion.update();
+		robotDrive.mecanumDrive_Cartesian(x, y, rotation, gyroAngle);
 	}
 	public void checkInput(){
 		

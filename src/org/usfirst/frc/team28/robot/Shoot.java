66f5ced44f4d8e44b6b1fce8486b381a78255091
Colby Wilson <colbyw12@gmail.com>
2017-05-31 16:25:36 -0400

    shoter code!!

---
+++ src/org/usfirst/frc/team28/robot/Shoot.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team28.robot;
+
+import edu.wpi.first.wpilibj.Spark;
+
+public class Shoot {
+	double spinnermotor;
+	Spark augar;
+	double wheel;
+	public Shoot(int channel){
+	}
+}

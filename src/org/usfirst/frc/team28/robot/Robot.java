7cd5698651a72e1c8a5b2771437e49877233b7c6
DriverStation <frcteam28@gmail.com>
2017-05-31 14:40:18 -0400

    add

--- src/org/usfirst/frc/team28/robot/Robot.java
+++ src/org/usfirst/frc/team28/robot/Robot.java
@@ -1,8 +1,7 @@
 package org.usfirst.frc.team28.robot;
 
-import edu.wpi.first.wpilibj.AnalogPotentiometer;
-import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -14,75 +13,71 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * directory.
  */
 public class Robot extends IterativeRobot {
-   final String defaultAuto = "Default";
-   final String customAuto = "My Auto";
-   String autoSelected;
-   SendableChooser<String> chooser = new SendableChooser<>();
-   AnalogPotentiometer pot;
-   DigitalOutput led;
+	Hang hangmotor;
+	final String defaultAuto = "Default";
+	final String customAuto = "My Auto";
+	String autoSelected;
+	SendableChooser<String> chooser = new SendableChooser<>();
 
-   /**
-    * This function is run when the robot is first started up and should be
-    * used for any initialization code.
-    */
-   @Override
-   public void robotInit() {
-      chooser.addDefault("Default Auto", defaultAuto);
-      chooser.addObject("My Auto", customAuto);
-      SmartDashboard.putData("Auto choices", chooser);
-      pot = new AnalogPotentiometer(0);
-      led = new DigitalOutput(0);
-      led.enablePWM(pot.get());
-   }
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		chooser.addDefault("Default Auto", defaultAuto);
+		chooser.addObject("My Auto", customAuto);
+		SmartDashboard.putData("Auto choices", chooser);
+		hangmotor = new Hang(new Spark(6));
+	}
 
-   /**
-    * This autonomous (along with the chooser code above) shows how to select
-    * between different autonomous modes using the dashboard. The sendable
-    * chooser code works with the Java SmartDashboard. If you prefer the
-    * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-    * getString line to get the auto name from the text box below the Gyro
-    *
-    * You can add additional auto modes by adding additional comparisons to the
-    * switch structure below with additional strings. If using the
-    * SendableChooser make sure to add them to the chooser code above as well.
-    */
-   @Override
-   public void autonomousInit() {
-      autoSelected = chooser.getSelected();
-      // autoSelected = SmartDashboard.getString("Auto Selector",
-      // defaultAuto);
-      System.out.println("Auto selected: " + autoSelected);
-   }
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 *` getString line to get the auto name from the text box below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the
+	 * switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
+	 */
+	@Override
+	public void autonomousInit() {
+		autoSelected = chooser.getSelected();
+		// autoSelected = SmartDashboard.getString("Auto Selector",
+		// defaultAuto);
+		System.out.println("Auto selected: " + autoSelected);
+	}
 
-   /**
-    * This function is called periodically during autonomous
-    */
-   @Override
-   public void autonomousPeriodic() {
-      switch (autoSelected) {
-      case customAuto:
-         // Put custom auto code here
-         break;
-      case defaultAuto:
-      default:
-         // Put default auto code here
-         break;
-      }
-   }
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		switch (autoSelected) {
+		case customAuto:
+			// Put custom auto code here
+			break;
+		case defaultAuto:
+		default:
+			// Put default auto code here
+			break;
+		}
+	}
 
-   /**
-    * This function is called periodically during operator control
-    */
-   @Override
-   public void teleopPeriodic() {
-      led.updateDutyCycle(pot.get());
-   }
+	/**
+	 * This function is called periodically during operator control
+	 */
+	@Override
+	public void teleopPeriodic() {
+	}
 
-   /**
-    * This function is called periodically during test mode
-    */
-   @Override
-   public void testPeriodic() {
-   }
+	/**
+	 * This function is called periodically during test mode
+	 */
+	@Override
+	public void testPeriodic() {
+	}
 }
 
